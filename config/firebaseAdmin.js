2a5060790cd83e43b147c241cf975e1d0f2a58b1
Kartik Sacesta <Kartik.sacesta@gmail.com>
2025-07-15 19:08:24 +0530

    deploy firebase eror fix

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../../firebasebackendconfig.json");
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY_JSON;
 try {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
